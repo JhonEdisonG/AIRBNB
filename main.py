c77229a31f70110602fd0fc544e04fc903b9a60d
JhonEdisonG <jhonedison240@gmail.com>
2025-02-08 18:22:38 -0500

    commit

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ app = FastAPI()
 SUPABASE_URL = "https://otaedzxedjzadltjtvzu.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90YWVkenhlZGp6YWRsdGp0dnp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwNTA1NDUsImV4cCI6MjA1NDYyNjU0NX0.mPZLDBWtfzcq-rEatzAlzWGrwggABZlmLP4EyL1VfZ4"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
 @app.get("/")
 def home():
     return FileResponse("PAGINAS/page.html")
+
+#------------------------------------------------------------------
+
+
+
 
 @app.get("/propiedades")
 def get_propiedades():
